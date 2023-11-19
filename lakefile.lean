35748863b51f0048b98f1484b37efd44a267824d
ksrk <example@example.com>
2023-11-19 12:56:39 +0900

    feat: remove unused files

--- lakefile.lean
+++ lakefile.lean
@@ -9,11 +9,3 @@ require mathlib from git
 
 lean_lib «Plfl» where
   -- add library configuration options here
-
-@[default_target]
-lean_exe «plfl» where
-  root := `Main
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  supportInterpreter := true
